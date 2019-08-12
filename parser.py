9ea6c6c0f757c36640eb7059b98d420b495457b1
maromoz <maromohevzion@gmail.com>
2019-08-12 10:39:32 +0300

    Second Commit + Tests

--- parser.py
+++ parser.py
@@ -2,70 +2,108 @@ import os
 import json
 import pyshark
 import requests
+import time
 
 
-
-def parser(pcap_file):
-    # Opening the captured file path given by the user:
-
+def dump_output_to_json(output, pcap_file):
     pcap_file_name = os.path.splitext(pcap_file)[0]
-    pcap_file_name_without_extension = os.path.basename(pcap_file_name)
-    cap = pyshark.FileCapture(pcap_file)
-    i = 1
-    for pkt in cap:
+    with open(pcap_file_name + '.json', "a") as json_file:
+        json_file.write("{}\n".format(json.dumps(output, indent=5)))
+
+
+def get_basic_packet_parameters(pkt):
+    try:
+        packet_number = pkt.number
         highest_layer = pkt.highest_layer
         src_ip = pkt.ip.src
         dst_ip = pkt.ip.dst
         src_port = pkt.tcp.srcport
         dst_port = pkt.tcp.dstport
+        return packet_number, highest_layer, src_ip, dst_ip, src_port, dst_port
+
+    except Exception as e:
+        print("Broken Packet", e)
+        return 0, 0, 0, 0, 0, 0
+
+
+def api_request_to_virus_total(website_url, urls_scanned):
+    if website_url == '':
+        return 0
+    if website_url in urls_scanned:
+        return urls_scanned[website_url]
+    try:
+        status_code = 204
+        while status_code == 204:
+            headers = {
+                "Accept-Encoding": "gzip, deflate",
+                "User-Agent": "gzip,  My Python requests library example client or username"
+            }
+            params = {'apikey': '3b8871665936a18f433ae5eee608148a4ed00ca69b9925cc22ab3ab414efa3c1',
+                      'resource': website_url}
+            response = requests.post("https://www.virustotal.com/vtapi/v2/url/report",
+                                     params=params, headers=headers)
+            if response.status_code == 204:
+                print("API's Limit Reached, waiting a few seconds")
+                time.sleep(15)
+            else:
+                status_code = response.status_code
+                urls_scanned[website_url] = response
+        return response
+
+    except Exception as e:
+        print("Problem with the API request", e.__repr__())
+        return 0
+
+
+def parsing_api_scan_results(response):
+    json_response = response.json()
+    scan_results = ''
+    # Checking in the json response from virustotal if a scan resulted in a malware/malicious result,
+    # if so, the result is appended to the scan results variable
+    for scan in json_response['scans']:
+        scan_result = json_response['scans'][scan]['result']
+        if scan_result != 'clean site' and scan_result != 'unrated site':
+            scan_results += scan + ' - ' + scan_result + ", "
+    if scan_results == '':
+        scan_results = 'Clean site'
+        print('The site ' + json_response['resource'] + ' is clean ')
+    scan_results = scan_results.rstrip(' ,')
+    return scan_results
+
+
+def main(pcap_file):
+    # Opening the captured file path given by the user:
+    cap = pyshark.FileCapture(pcap_file)
+    output = []
+    urls_scanned = {}
+    for pkt in cap:
+        packet_number, highest_layer, src_ip, dst_ip, src_port, dst_port = get_basic_packet_parameters(pkt)
+        if highest_layer == 0:
+            continue
+        packet_data = {
+            'Packet number': packet_number,
+            'Protocol': highest_layer,
+            'Source ip': src_ip,
+            'Destination ip': dst_ip,
+            'Source port': src_port,
+            'Destination port': dst_port
+        }
         if highest_layer == 'HTTP':
             try:
                 website_url = pkt.http.host
-                headers = {
-                    "Accept-Encoding": "gzip, deflate",
-                    "User-Agent": "gzip,  My Python requests library example client or username"
-                }
-                params = {'apikey': '3b8871665936a18f433ae5eee608148a4ed00ca69b9925cc22ab3ab414efa3c1', 'resource': website_url}
-                response = requests.post('https://www.virustotal.com/vtapi/v2/url/report',
-                                         params=params, headers=headers)
-                json_response = response.json()
-                scan_results = ''
+            except Exception as e:
+                print("Packet has no url")
+                continue
+            response = api_request_to_virus_total(website_url, urls_scanned)
+            if response == 0:
+                continue
+            api_scan_results = parsing_api_scan_results(response)
+            packet_data['Scan results'] = api_scan_results
+            packet_data['Url'] = website_url
+        output.append(packet_data)
 
-                # Checking in the json response from virustotal if a scan resulted in a malware/malicious result,
-                # if so, the result is appended to the scan results variable
+    dump_output_to_json(output, pcap_file)
 
-                for scan in json_response['scans']:
-                    scan_result = json_response['scans'][scan]['result']
-                    if scan_result != 'clean site' and scan_result != 'unrated site':
-                        scan_results += scan + ' - ' + scan_result + ", "
-                if scan_results == '':
-                    scan_results = 'Clean site'
-                scan_results = scan_results.rstrip(' ,')
-                temp_dict = {
-                    'Scan results': scan_results,
-                    'Packet number': i,
-                    'Protocol': highest_layer,
-                    'Source ip': src_ip,
-                    'Destination ip': dst_ip,
-                    'Source port': src_port,
-                    'Destination port': dst_port,
-                    'Url': website_url
-                }
-            except:
-                print "Oops, the HTTP packet was broken!"
-        else:
-            temp_dict = {
-                'Packet number': i,
-                'Protocol': highest_layer,
-                'Source ip': src_ip,
-                'Destination ip': dst_ip,
-                'Source port': src_port,
-                'Destination port': dst_port
-            }
 
-        with open(pcap_file_name_without_extension+'.json', "a") as json_file:
-            json_file.write("{}\n".format(json.dumps(temp_dict, indent=5)))
-
-        i += 1
-
-parser('/home/director/tcpdump7.pcap')
+if __name__ == '__main__':
+    main('MalwareTrafficPcap.pcap')
---
+++ test_parser.py
@@ -0,0 +1,122 @@
+import pyshark
+import requests
+import os
+import json
+import pandas as pd
+
+
+# get_basic_packet_parameters tests
+def get_basic_packet_parameters(pkt):
+    packet_number = pkt.number
+    highest_layer = pkt.highest_layer
+    src_ip = pkt.ip.src
+    dst_ip = pkt.ip.dst
+    src_port = pkt.tcp.srcport
+    dst_port = pkt.tcp.dstport
+    return packet_number, highest_layer, src_ip, dst_ip, src_port, dst_port
+
+
+def test_get_basic_packet_parameters_with_bad_pcap_file(bad_pacp_file='bad.pcap'):
+    bad_pacp_file = pyshark.FileCapture(bad_pacp_file)
+    for pkt in bad_pacp_file:
+        result = get_basic_packet_parameters(pkt)
+    assert result
+
+
+def test_get_basic_packet_parameters_with_goodpcap_file(good_pacp_file='good.pcap'):
+    good_pacp_file = pyshark.FileCapture(good_pacp_file)
+    for pkt in good_pacp_file:
+        result = get_basic_packet_parameters(pkt)
+    assert result
+
+
+# api_request_to_virus_total tests
+def api_request_to_virus_total(website_url):
+    if website_url == '':
+        raise Exception
+    headers = {
+        "Accept-Encoding": "gzip, deflate",
+        "User-Agent": "gzip,  My Python requests library example client or username"
+    }
+    params = {'apikey': '3b8871665936a18f433ae5eee608148a4ed00ca69b9925cc22ab3ab414efa3c1',
+              'resource': website_url}
+    response = requests.post("https://www.virustotal.com/vtapi/v2/url/report",
+                             params=params, headers=headers)
+
+
+def test_api_request_to_virus_total_with_no_url(empty_url=''):
+    api_request_to_virus_total(empty_url)
+
+
+def test_api_request_to_virus_total_with_good_url(good_url='google.com'):
+    api_request_to_virus_total(good_url)
+
+
+# parsing_api_scan_results tests
+def parsing_api_scan_results(response):
+    json_response = response.json()
+    scan_results = ''
+    # Checking in the json response from virustotal if a scan resulted in a malware/malicious result,
+    # if so, the result is appended to the scan results variable
+    for scan in json_response['scans']:
+        scan_result = json_response['scans'][scan]['result']
+        if scan_result != 'clean site' and scan_result != 'unrated site':
+            scan_results += scan + ' - ' + scan_result + ", "
+    if scan_results == '':
+        scan_results = 'Clean site'
+    scan_results = scan_results.rstrip(' ,')
+    return scan_results
+
+
+def test_parsing_api_scan_results_with_bad_response_as_parameter(bad_response=1):
+    result = parsing_api_scan_results(bad_response)
+    assert result
+
+
+def test_parsing_api_scan_results_with_good_response_as_parameter(good_response=''):
+    headers = {
+        "Accept-Encoding": "gzip, deflate",
+        "User-Agent": "gzip,  My Python requests library example client or username"
+    }
+    params = {'apikey': '3b8871665936a18f433ae5eee608148a4ed00ca69b9925cc22ab3ab414efa3c1',
+              'resource': 'google.com'}
+    good_response = requests.post("https://www.virustotal.com/vtapi/v2/url/report",
+                                  params=params, headers=headers)
+    result = parsing_api_scan_results(good_response)
+    assert result
+
+
+# dump_output_to_json tests
+def dump_output_to_json(output, pcap_file):
+    pcap_file_name = os.path.splitext(pcap_file)[0]
+    with open(pcap_file_name + '.json', "a") as json_file:
+        json_file.write("{}\n".format(json.dumps(output, indent=5)))
+
+
+def test_dump_output_to_json_with_bad_output(bad_output=pd.DataFrame, pcap_file='good.pcap'):
+    dump_output_to_json(bad_output, pcap_file)
+
+
+def test_dump_output_to_json_with_good_output(good_output=[], pcap_file='good.pcap'):
+    good_output = [{'Packet number': '1',
+                    'Protocol': 'TCP',
+                    'Source ip': '10.60.6.154',
+                    'Destination ip': '1.2.3.4',
+                    'Source port': '20',
+                    'Destination port': '123'}]
+    dump_output_to_json(good_output, pcap_file)
+
+
+# main tests, check if the given file has .pcap extension
+def main(pcap_file):
+    cap = pyshark.FileCapture(pcap_file)
+
+
+def test_main_with_bad_pcap_file(bad_file='bad.pcapa'):
+    pcap_file_extension = os.path.splitext(bad_file)[1]
+    assert pcap_file_extension == '.pcap'
+
+
+def test_main_with_good_pcap_file(good_file='good.pcap'):
+    pcap_file_extension = os.path.splitext(good_file)[1]
+    assert pcap_file_extension == '.pcap'
